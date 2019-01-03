b6cea387887512b818fa959d291502fe1bd0872d
Rafael Redondo <bruceramone@gmail.com>
2019-01-03 12:04:19 -0200

    Adicionando conexão com o banco e mapeamento

---
+++ src/main/java/com/rafaelredondo/ponto/api/enums/PerfilEnum.java
@@ -0,0 +1,8 @@
+package com.rafaelredondo.ponto.api.enums;
+
+public enum PerfilEnum {
+	
+	ROLE_ADMIN,
+	ROLE_USUARIO
+
+}
---
+++ src/main/java/com/rafaelredondo/ponto/api/enums/TipoEnum.java
@@ -0,0 +1,12 @@
+package com.rafaelredondo.ponto.api.enums;
+
+public enum TipoEnum {
+	
+	INICIO_TRABALHO,
+	TERMINO_TRABALHO,
+	INICIO_ALMOCO,
+	TERMINO_ALMOCO,
+	INICIO_PAUSA,
+	TERMINO_PAUSA
+
+}
